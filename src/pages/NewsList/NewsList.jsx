35b0a49256a228ac654312234dd3a3a0de94ef7c
Daniel Merritt <daniel.mafou@gmail.com>
2023-10-09 11:02:37 -0400

    pull from api on front end

--- src/pages/NewsList/NewsList.jsx
+++ src/pages/NewsList/NewsList.jsx
@@ -1,9 +1,24 @@
+// services
+import * as newsService from '../../services/newsService'
+
+// npm modules
+import { useState, useEffect } from 'react'
+
 const NewsList = () => {
+
+  const [news, setNews] = useState([])
+
+  // const fetchNews = async () => {
+  //   const data = await newsService.index()
+  //   setNews(data)
+  // }
+  // fetchNews()
+
   return ( 
     <>
       <div>NewsList</div>
     </>
   );
 }
- 
+
 export default NewsList;
