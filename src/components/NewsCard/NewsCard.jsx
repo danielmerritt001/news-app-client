749a1d84ceaea6a2f15095d520d33dfae6be71ad
Daniel Merritt <daniel.mafou@gmail.com>
2023-10-11 10:47:14 -0400

    adjust original layout based on length of news titles

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -3,11 +3,11 @@ import styles from './NewsCard.module.css'
 const NewsCard = (props) => {
   return ( 
       <article className={styles.newsCard}>
-        <header className={styles.title}>{props.article.title}</header>
         <a href={props.article.url} target='_blank' rel='noreferrer'>
           <img className={styles.image} src={props.article.urlToImage} alt="" />
         </a>
-        <p className={styles.content}>{props.article.content}</p>
+        <header className={styles.title}>{props.article.title}</header>
+        {/* <p className={styles.content}>{props.article.content}</p> */}
       </article>
   );
 }
